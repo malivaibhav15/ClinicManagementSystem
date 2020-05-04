aae6739616aea442b79761f030a654fbdf3e8496
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-04 12:46:52 +0530

    ADDED : POJO class for doctor

--- src/main/java/com/BridgeLabz/Model/Doctor.java
+++ src/main/java/com/BridgeLabz/Model/Doctor.java
@@ -1,4 +1,51 @@
 package com.BridgeLabz.Model;
 
-public class Doctor {
+public class Doctor
+{
+    private String name;
+    private String id;
+    private String specialization;
+     private  String schedule;
+
+    public Doctor() {
+    }
+
+    public Doctor(String name, String id, String specialization, String schedule) {
+        this.name = name;
+        this.id = id;
+        this.specialization = specialization;
+        this.schedule = schedule;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
 }
