f8f5bc436655ae6871c993fd90e83139da3eb839
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-04 12:14:48 +0530

    ADDED : Welcome message

--- src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
+++ src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
@@ -1,4 +1,7 @@
 package com.BridgeLabz.Service;
 
 public class ClinicManagementSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to clinic management system");
+    }
 }
