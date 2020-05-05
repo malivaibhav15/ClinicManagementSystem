331767366409b2bec3b4f4682eea7e505bf1ed98
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-05 15:46:23 +0530

    ADDED : Method to read and write into the files of patient

--- src/main/java/com/BridgeLabz/Service/FileSystem.java
+++ src/main/java/com/BridgeLabz/Service/FileSystem.java
@@ -1,6 +1,7 @@
 package com.BridgeLabz.Service;
 
 import com.BridgeLabz.Model.Doctor;
+import com.BridgeLabz.Model.Patient;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -8,16 +9,26 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class FileSystem
-{
+public class FileSystem {
     ObjectMapper objectMapper = new ObjectMapper();
 
-    public ArrayList<Doctor> readFile(String filePath) throws IOException {
-        ArrayList<Doctor> data = objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Doctor>>(){});
+    public ArrayList<Doctor> readFileDoctor(String filePath) throws IOException {
+        ArrayList<Doctor> data = objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Doctor>>() {});
         return data;
     }
 
-    public void writeFile(ArrayList<Doctor> list, String filePath) throws IOException {
+    public void writeFileDoctor(ArrayList<Doctor> list, String filePath) throws IOException {
         objectMapper.writeValue(new File(filePath), list);
     }
+
+    public ArrayList<Patient> readFilePatient(String filePath) throws IOException {
+        ArrayList<Patient> data = objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Patient>>() {});
+        return data;
+    }
+
+    public void writeFilePatient(ArrayList<Patient> list, String filePath) throws IOException {
+        objectMapper.writeValue(new File(filePath), list);
+    }
+
+
 }
