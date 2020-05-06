db9f1f114fed23f6fdaa103b5305701a49556eb4
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-06 13:24:26 +0530

    ADDED : Methods for updating the doctors and patients information

--- src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
+++ src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
@@ -20,9 +20,29 @@ public class ClinicManagementSystemMain implements DoctorInterface, PatientInter
     }
 
     @Override
+    public void updateDoctor(Doctor doctor, String doctorFilePath) throws IOException {
+        ArrayList<Doctor> doctorArrayList = fileSystem.readFileDoctor(doctorFilePath);
+        for (Doctor doctor1 : doctorArrayList) {
+            if (doctor1.getId().equalsIgnoreCase(doctor.getId()))
+                doctor1.setSchedule(doctor.getSchedule());
+            fileSystem.writeFileDoctor(doctorArrayList, doctorFilePath);
+        }
+    }
+
+    @Override
     public void addPatient(Patient patient, String filePath) throws IOException {
         ArrayList<Patient> data = fileSystem.readFilePatient(filePath);
         data.add(patient);
         fileSystem.writeFilePatient(data, filePath);
+    }
+
+    @Override
+    public void updatePatient(Patient patient, String patientFilePath) throws IOException {
+     ArrayList<Patient> data = fileSystem.readFilePatient(patientFilePath);
+        for (Patient patient1 : data) {
+            if (patient1.getId().equals(patient.getId()))
+                patient1.setMobileNumber(patient.getMobileNumber());
+            fileSystem.writeFilePatient(data, patientFilePath);
+        }
     }
 }
