c82681948d4b6427307107f69f23bbdb78b884f4
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-06 11:19:32 +0530

    ADDED : Methods for adding patient and doctor

---
+++ src/main/java/com/BridgeLabz/Interface/PatientInterface.java
@@ -0,0 +1,4 @@
+package com.BridgeLabz.Interface;
+
+public interface PatientInterface {
+}
--- src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
+++ src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
@@ -1,7 +1,28 @@
 package com.BridgeLabz.Service;
 
-public class ClinicManagementSystemMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to clinic management system");
+import com.BridgeLabz.Interface.DoctorInterface;
+import com.BridgeLabz.Interface.PatientInterface;
+import com.BridgeLabz.Model.Doctor;
+import com.BridgeLabz.Model.Patient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ClinicManagementSystemMain implements DoctorInterface, PatientInterface {
+
+    FileSystem fileSystem = new FileSystem();
+
+    @Override
+    public void addDoctor(Doctor doctor, String filePath) throws IOException {
+        ArrayList<Doctor> data = fileSystem.readFileDoctor(filePath);
+        data.add(doctor);
+        fileSystem.writeFileDoctor(data, filePath);
+    }
+
+    @Override
+    public void addPatient(Patient patient, String filePath) throws IOException {
+        ArrayList<Patient> data = fileSystem.readFilePatient(filePath);
+        data.add(patient);
+        fileSystem.writeFilePatient(data, filePath);
     }
 }
