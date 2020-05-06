cdbe048e7a0f658cd085b7c7ceaa0d5c3db3c101
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-06 13:25:25 +0530

    ADDED : Mathod decleration to update doctor

--- src/main/java/com/BridgeLabz/Interface/DoctorInterface.java
+++ src/main/java/com/BridgeLabz/Interface/DoctorInterface.java
@@ -2,8 +2,11 @@ package com.BridgeLabz.Interface;
 
 import com.BridgeLabz.Model.Doctor;
 
+import java.io.IOException;
+
 public interface DoctorInterface
 {
-    void addDoctor(Doctor doctor, String filePath);
+    void addDoctor(Doctor doctor, String filePath) throws IOException;
 
+    void updateDoctor(Doctor doctor, String doctorFilePath) throws IOException;
 }
