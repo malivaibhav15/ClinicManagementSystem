e382adb0338e103065d673931dc39a98fe5290c9
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-07 14:51:10 +0530

    ADDED : Method to get the list of doctors and patients

--- src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
+++ src/main/java/com/BridgeLabz/Service/ClinicManagementSystemMain.java
@@ -1,5 +1,6 @@
 package com.BridgeLabz.Service;
 
+import com.BridgeLabz.Interface.ClinicManagementSystemInterface;
 import com.BridgeLabz.Interface.DoctorInterface;
 import com.BridgeLabz.Interface.PatientInterface;
 import com.BridgeLabz.Model.Doctor;
@@ -7,8 +8,9 @@ import com.BridgeLabz.Model.Patient;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
-public class ClinicManagementSystemMain implements DoctorInterface, PatientInterface {
+public class ClinicManagementSystemMain implements DoctorInterface, PatientInterface, ClinicManagementSystemInterface {
 
     FileSystem fileSystem = new FileSystem();
 
@@ -38,11 +40,36 @@ public class ClinicManagementSystemMain implements DoctorInterface, PatientInter
 
     @Override
     public void updatePatient(Patient patient, String patientFilePath) throws IOException {
-     ArrayList<Patient> data = fileSystem.readFilePatient(patientFilePath);
+        ArrayList<Patient> data = fileSystem.readFilePatient(patientFilePath);
         for (Patient patient1 : data) {
             if (patient1.getId().equals(patient.getId()))
                 patient1.setMobileNumber(patient.getMobileNumber());
             fileSystem.writeFilePatient(data, patientFilePath);
         }
     }
+
+    @Override
+    public boolean listOfPatient(String patientFilePath) {
+        try {
+            ArrayList<Patient> patientArrayList = fileSystem.readFilePatient(patientFilePath);
+            patientArrayList.forEach((patientList) -> System.out.println(patientList));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean listOfDoctors(String doctorFilePath) {
+        try {
+            ArrayList<Doctor> doctorArrayList = fileSystem.readFileDoctor(doctorFilePath);
+            doctorArrayList.forEach((doctorList) -> System.out.println(doctorList));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
