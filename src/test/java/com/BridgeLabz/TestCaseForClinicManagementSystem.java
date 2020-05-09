866a8f586ea9016c08afd23750fb32fd8373ea5b
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-09 11:15:00 +0530

    ADDED : Test case for taking appointment

--- src/test/java/com/BridgeLabz/TestCaseForClinicManagementSystem.java
+++ src/test/java/com/BridgeLabz/TestCaseForClinicManagementSystem.java
@@ -1,5 +1,6 @@
 package com.BridgeLabz;
 
+import com.BridgeLabz.Model.Appointment;
 import com.BridgeLabz.Model.Doctor;
 import com.BridgeLabz.Model.Patient;
 import com.BridgeLabz.Service.ClinicManagementSystemMain;
@@ -16,6 +17,7 @@ public class TestCaseForClinicManagementSystem {
 
     String doctorFilePath = "src/main/resources/AddDoctor.json";
     String patientFilePath = "src/main/resources/AddPatient.json";
+    String appointmentFilePath = "src/main/resources/AddAppointment.json";
     ClinicManagementSystemMain clinicManagementSystemMain = new ClinicManagementSystemMain();
     ObjectMapper objectMapper = new ObjectMapper();
 
@@ -159,4 +161,24 @@ public class TestCaseForClinicManagementSystem {
         Assert.assertEquals(doctor4.getName(), data.get(3).getName());
     }
 
+    @Test
+    public void givenWhenUserTakesAppointment_WhenProper_ShouldReturnTrue() throws IOException {
+        Appointment appointment1 = new Appointment("Vaibhav", "Abc", "10-11");
+        Appointment appointment2 = new Appointment("Vaibhav", "Pqr", "10-11");
+        Appointment appointment3 = new Appointment("Vaibhav", "Mno", "10-11");
+        Appointment appointment4 = new Appointment("Vaibhav", "Xyz", "10-11");
+        Appointment appointment5 = new Appointment("Vaibhav", "Gef", "10-11");
+        clinicManagementSystemMain.addAppointment(appointment1, appointmentFilePath);
+        clinicManagementSystemMain.addAppointment(appointment2, appointmentFilePath);
+        clinicManagementSystemMain.addAppointment(appointment3, appointmentFilePath);
+        clinicManagementSystemMain.addAppointment(appointment4, appointmentFilePath);
+        clinicManagementSystemMain.addAppointment(appointment5, appointmentFilePath);
+        ArrayList<Appointment> data = objectMapper.readValue(new File(appointmentFilePath), new TypeReference<ArrayList<Appointment>>(){});
+        Assert.assertEquals(appointment1.getDoctorName(), data.get(0).getDoctorName());
+        Assert.assertEquals(appointment2.getDoctorName(), data.get(1).getDoctorName());
+        Assert.assertEquals(appointment3.getDoctorName(), data.get(2).getDoctorName());
+        Assert.assertEquals(appointment4.getDoctorName(), data.get(3).getDoctorName());
+        Assert.assertEquals(appointment5.getDoctorName(), data.get(4).getDoctorName());
+
+    }
 }
